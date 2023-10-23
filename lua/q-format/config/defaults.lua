1e060291821ef07b8a46766c0d1bedc853160976
Futar <Futarimiti@163.com>
2023-10-23 18:07:39 +0100

    add default opts

--- lua/q-format/config/defaults.lua
+++ lua/q-format/config/defaults.lua
@@ -1,10 +1,18 @@
 local typecheck = function (config)
   vim.validate
   { config = { config, 'table' }
+  , ['config.custom'] = { config.custom, 'table' }
+  , ['config.preferences'] = { config.preferences, 'table' }
+  , ['config.preferences.*'] = { config.preferences['*'], 'table' }
   }
 end
 
-local defaults = {}
+local e = require 'q-format.formatters'
+
+local defaults =
+{ custom = {}  -- custom formatters, in the format that you would pass to formatprg
+, preferences = { ['*'] = { e.CUSTOM, e.FORMATPRG, e.FORMATEXPR } }  -- preferences of formatters for each filetype
+}
 
 assert(pcall(typecheck, defaults))
 
