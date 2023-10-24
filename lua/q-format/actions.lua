c16a0d0c3ddc01af35360f9a1d6c03fbce2d4aac
Futar <Futarimiti@163.com>
2023-10-24 23:00:56 +0100

    remove unused funcs

--- lua/q-format/actions.lua
+++ lua/q-format/actions.lua
@@ -71,45 +71,4 @@ M.eq = function (buf)
   vim.api.nvim_buf_call(buf, cmd)
 end
 
--- place cursor
--- will be placed at the last row/col if out-of-bound
-M.cursor = function (win, pos)
-  local buf = vim.api.nvim_win_get_buf(win)
-  local row, col = unpack(pos)
-  local dest_row = math.min(row, vim.api.nvim_buf_line_count(buf))
-  local line = vim.api.nvim_buf_get_lines(buf, dest_row - 1, dest_row, false)[1] or ''
-  local dest_col = math.min(col, #line)
-  vim.api.nvim_win_set_cursor(0, { dest_row, dest_col })
-end
-
-M.get_cursor = function (win)
-  return vim.api.nvim_win_get_cursor(win)
-end
-
-M.with_cursor_in_place = function (win, f)
-  local pos = M.get_cursor(win)
-  local successful, errmsg = pcall(f)
-  M.cursor(win, pos)
-  if not successful then error(errmsg) end
-end
-
--- copy contents from one buffer to another
-M.bufcopy = function (from_buf, to_buf)
-  local contents = vim.api.nvim_buf_get_lines(from_buf, 0, -1, false)
-  vim.api.nvim_buf_set_lines(to_buf, 0, -1, false, contents)
-end
-
--- cps
-M.with_tempbuf = function (f)
-  local tempbuf = vim.api.nvim_create_buf(false, true)
-  local successful, errmsg = pcall(f, tempbuf)
-
-  if vim.api.nvim_buf_is_valid(tempbuf) then
-    vim.api.nvim_buf_delete(tempbuf, { force = true })
-  end
-
-  -- rethrow error
-  if not successful then error(errmsg) end
-end
-
 return M
