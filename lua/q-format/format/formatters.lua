96ff7465e125695c6ad8dfad3db185fd41834c26
Futar <Futarimiti@163.com>
2023-11-02 11:18:22 +0000

    drop needy actions module

--- lua/q-format/format/formatters.lua
+++ lua/q-format/format/formatters.lua
@@ -1,37 +1,98 @@
 -- formatters
 -- some are in CPS --- what to do on success and failure
--- will move cursor
+-- most will move cursor
 
 local M = {}
 
-local a = require 'q-format.actions'
 local formatters = require 'q-format.formatters'
 
+local lines = function (buf)
+  return vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+end
+
+local contents = function (buf)
+  return table.concat(lines(buf), '\n')
+end
+
+local set_lines = function (buf, lines_)
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines_)
+end
+
+local show_formatter = function (formatter)
+  if formatter == formatters.CUSTOM then
+    return 'custom'
+  elseif formatter == formatters.EQUALPRG then
+    return 'equalprg'
+  elseif formatter == formatters.FORMATPRG then
+    return 'formatprg'
+  elseif formatter == formatters.FORMATEXPR then
+    return 'formatexpr'
+  end
+end
+
+local normal = function (keys)
+  return function ()
+    vim.api.nvim_cmd(
+       { cmd = 'normal'
+       , args = { keys }
+       , bang = true
+       , mods = { silent = true }
+       }, {})
+  end
+end
+
+-- normal! gq for the whole buffer
+-- NOTE: cursor will be moved to top of buffer DURING formatting
+-- NOTE: hence do expect transient cut screen when using external formatters
+-- NOTE: use M.format_external for better experience
+local gq = function (buf)
+  local cmd = normal 'gggqG'
+  vim.api.nvim_buf_call(buf, cmd)
+end
+
+-- format the whole buffer using external formatter
+-- formatter should be in style of formatprg
+-- NOTE: cursor will be moved to top of buffer after formatting
+local format_external = function (buf_, formatter_)
+  local cmd = function ()
+    vim.cmd('%!' .. formatter_)
+  end
+  vim.api.nvim_buf_call(buf_, cmd)
+end
+
+-- normal! = for the whole buffer
+-- NOTE: cursor will be moved to top of buffer after formatting
+local eq = function (buf_)
+  local cmd = normal 'gg=G'
+  vim.api.nvim_buf_call(buf_, cmd)
+end
+
+-------------------------------------------------------------------------------------
+
 local equalprg = function (_, buf, on_success, _)
-  a.eq(buf)
+  eq(buf)
   on_success(buf)
 end
 
 local formatexpr = function (_, buf, on_success, _)
   assert(vim.api.nvim_buf_get_option(buf, 'formatexpr') ~= '', 'no formatexpr set')
-  a.gq(buf)
+  gq(buf)
   on_success(buf)
 end
 
 local format_with_formatter = function (_, buf, on_success, on_failure, formatter)
-  local backup = a.lines(buf)
+  local backup_lines = lines(buf)
 
-  a.format_external(buf, formatter)
+  format_external(buf, formatter)
   if vim.v.shell_error ~= 0 then
     -- what's currently on the buffer will be the error msg
     -- thank you, builtin formatter
-    local msg = a.contents(buf)
-    a.set_lines(buf, backup)
+    local msg = contents(buf)
+    set_lines(buf, backup_lines)
     on_failure(buf, msg)
   else
     on_success(buf)
   end
-
 end
 
 local formatprg = function (user, buf, on_success, on_failure)
@@ -51,25 +112,13 @@ local as_is = function (_, buf, on_success, _)
   on_success(buf)
 end
 
-local show = function (formatter)
-  if formatter == formatters.CUSTOM then
-    return 'custom'
-  elseif formatter == formatters.EQUALPRG then
-    return 'equalprg'
-  elseif formatter == formatters.FORMATPRG then
-    return 'formatprg'
-  elseif formatter == formatters.FORMATEXPR then
-    return 'formatexpr'
-  end
-end
-
 local select_formatter = function (user, buf)
   local notify = require('q-format.logger').notify
 
   local ft = vim.bo[buf].filetype
   local preferences = user.preferences[ft] or user.preferences['*']
 
-  notify('[q-format] formatter preferences for ' .. ft .. ': ' .. vim.inspect(vim.tbl_map(show, preferences)))
+  notify('[q-format] formatter preferences for ' .. ft .. ': ' .. vim.inspect(vim.tbl_map(show_formatter, preferences)))
 
   if vim.tbl_isempty(preferences) then
     notify('[q-format] no formatter preference set for ' .. ft .. ', no format')
--- lua/q-format/format/init.lua
+++ lua/q-format/format/init.lua
@@ -1,14 +1,25 @@
 local M = {}
 
-local a = require 'q-format.actions'
 local f = require 'q-format.format.formatters'
+
+local retaining_view = function (buf, f_)
+  vim.api.nvim_buf_call(buf, function ()
+    local tempname = vim.fn.tempname()
+    vim.cmd('silent mkview! ' .. tempname)
+    local successful, errmsg = pcall(f_)
+    vim.cmd('source ' .. tempname)
+    if not successful then
+      error(errmsg)
+    end
+  end)
+end
 
 -- view will not be updated throughout formatting, successful or not
 -- buffer will be :update'd only if without error
 M.format = function (user, win)
   local buf = vim.api.nvim_win_get_buf(win)
 
-  a.retaining_view(buf, function ()
+  retaining_view(buf, function ()
     f.format(user, buf)
   end)
 
